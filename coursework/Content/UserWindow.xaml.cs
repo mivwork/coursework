d9e703bfa4a16d1902287086570c1f87e859cef1
Dmitry1012 <dasergeev2003@mail.ru>
2024-04-25 16:55:58 +0300

    Окно профиля

---
+++ coursework/Content/UserWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace coursework.Content
+{
+    /// <summary>
+    /// Interaction logic for UserWindow
+    /// </summary>
+    public partial class UserWindow : UserControl
+    {
+        public UserWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
