12cb4a8d749c38137a406a9a1f50243892ae8004
mivwork <iv.menshev2003@gmail.com>
2024-05-18 19:45:35 +0300

    Добавлена выгрузка в Excel

--- Model2/ViewModels/TableViewForm.cs
+++ Model2/ViewModels/TableViewForm.cs
@@ -5,11 +5,14 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using OfficeOpenXml;
 
 namespace Model2.ViewModels;
 
@@ -18,7 +21,11 @@ internal class TableViewModel : BindableBase
     private readonly IRegionManager regionManager;
     private readonly ClockService clockService;
     public ICommand TableCommand { get; init; }
-    
+    public ICommand ExcelCommand { get; init; }
+    ExcelPackage package;
+
+    string projectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
     private List<Clock> _itemValue;
     public List<Clock> ItemValue
     {
@@ -55,11 +62,45 @@ internal class TableViewModel : BindableBase
         ItemShopValue = clockService.getDataShop();
         CountStr += ItemValue.Count().ToString();
         TableCommand = new DelegateCommand(getTable);
+        ExcelCommand = new DelegateCommand(excel);
     }
 
     private void getTable()
     {
         ItemValue = clockService.getDataClockShop(Shop);
         CountStr = "Количество строк: " + ItemValue.Count().ToString();
+    }
+
+    private void excel()
+    {
+        // Добавляем новый лист
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        package = new ExcelPackage();
+        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+        // Заголовки столбцов
+        worksheet.Cells[1, 1].Value = "id";
+        worksheet.Cells[1, 2].Value = "Бренд";
+        worksheet.Cells[1, 3].Value = "Модель";
+        worksheet.Cells[1, 4].Value = "Город";
+        worksheet.Cells[1, 5].Value = "Цена";
+        worksheet.Cells[1, 6].Value = "Статус";
+        worksheet.Cells[1, 7].Value = "Магазин";
+
+        // Выводим данные в ячейки
+        for (int i = 0; i < ItemValue.Count; i++)
+        {
+            worksheet.Cells[i + 2, 1].Value = ItemValue[i].id;
+            worksheet.Cells[i + 2, 2].Value = ItemValue[i].brend;
+            worksheet.Cells[i + 2, 3].Value = ItemValue[i].model;
+            worksheet.Cells[i + 2, 4].Value = ItemValue[i].country;
+            worksheet.Cells[i + 2, 5].Value = ItemValue[i].cost;
+            worksheet.Cells[i + 2, 6].Value = ItemValue[i].status;
+            worksheet.Cells[i + 2, 7].Value = ItemValue[i].shop;
+        }
+
+        // Сохраняем файл Excel в папке с проектом
+        FileInfo file = new FileInfo(Path.Combine(projectPath, "Учет_часов.xlsx"));
+        package.SaveAs(file);
     }
 }
